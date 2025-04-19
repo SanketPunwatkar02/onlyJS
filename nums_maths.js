1e523f834ac5b68155f2233aca450cc7334e91a9
SanketPunwatkar02 <sanketpunwatkar02@gmail.com>
2025-04-19 20:37:58 +0530

    new file:   nums_maths.js 	modified:   test.js

---
+++ nums_maths.js
@@ -0,0 +1,18 @@
+const score =400
+// console.log(score)
+const bal = new Number(100)
+// console.log(bal)
+// console.log(bal.toString())
+// console.log(bal.toFixed(2))
+const num = 1000000000
+// console.log(num.toLocaleString('en-IN'))
+
+//////////////////////////////-MATHS-//////////
+
+// console.log(Math)
+// let sam = 16
+// console.log(Math.sqrt(sam)) /// there  are many math functions check it on inspect->console->console.log(Math)
+
+console.log(Math.random())
+
+console.log((Math.random()*10)+1 ) 
--- test.js
+++ test.js
@@ -1,6 +1,12 @@
-something ="kuch"
+const something ="kuch"
 is =  "nhi"
 everything ="hai"
-console.log("hello sanket")
-console.table([something,is,everything])
-console.log("hey Sanket")
+// console.log("hello sanket")
+// console.log('this is ${something}');
+// console.table([something,is,everything])
+// console.log("hey Sanket")
+console.log(`hello my thing ${something}`);
+const sam = new String("Sanket");
+console.log(sam.toUpperCase())
+console.log(sam);
+console.log(sam.split(2))
